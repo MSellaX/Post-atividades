5054d60013982fd86dc8f88eeb75618b36f2275e
unknown <you@example.com>
2025-09-16 13:01:32 -0300

    POST

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 //atividade 3
 
-const { notEqual } = require('assert');
 const express = require('express'); 
 const app = express();
 const PORT = 8081;
@@ -11,23 +10,14 @@ app.use(express.json());
 
 app.post(`/soma`, (req, res ) => { // Rota para adicionar usuários
     try {
-        const {} = req.body; //extrai numeros do corpo da requisição
-        if (!numeros) {
-            return res.status(400).send("Nome e notas são obrigatórios");
+        const {numeros} = req.body; //extrai numeros do corpo da requisição
+        console.log(numeros);
+        const soma = numeros.reduce((valorAcumulado, atual) => valorAcumulado + atual, 0);
+       
+        if(Number, isNaN(numeros)){
+            return res.status(400).send("coloque apenas numeros ");
         }
-
-        const soma = somaFinal.reduce((valorAcumulado, nota) => valorAcumulado + nota);
-        const media = soma ;
-
-        const somaFinal = {numeros};
-        isNaN(somaFinal) ? res.status(400).send("Erro: algum valor não é um número"):
-        res.status(200).json(mediaFinal);
-
-        let medias = []; //array para armazenar as médias
-            const data = fs.readFileSync('medias.json', 'utf-8'); //lê o arquivo medias.json
-            medias = JSON.parse(data);       
-
-        res.status(201).json(mediaFinal);
+        res.status(200).json({resultado: soma});
 
     } catch (error) { //tratamento de erro
         console.error("Erro ao processar a requisição:", error);
@@ -37,6 +27,4 @@ app.post(`/soma`, (req, res ) => { // Rota para adicionar usuários
 
 app.listen(PORT, () => {  //inicia o servidor
     console.log(`Servidor rodando na porta HTTP://localhost:${PORT}`);
-
-
 });
