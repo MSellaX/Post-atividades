4621f054c120680c49e5e8ad459aa1ab40df34a4
unknown <you@example.com>
2025-09-16 12:19:12 -0300

    POST

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-//atividade 2
+//atividade 2 
 
+const { notEqual } = require('assert');
 const express = require('express'); 
 const app = express();
 const PORT = 8081;
@@ -7,15 +8,26 @@ const fs = require('fs');
 
 app.use(express.json());
 
-app.post(`/usuarios/`, (req, res ) => { // Rota para adicionar usuários
+app.post(`/medias`, (req, res ) => { // Rota para adicionar usuários
     try {
-        const {nome, nota, situacao} = req.body; //extrai nome e idade do corpo da requisição
-        if (nome == "" || nota == "") {
-            return res.status(400).send("Nome e nota são obrigatórios");
+        const {nome, notas} = req.body; //extrai nome e notas do corpo da requisição
+        if (!nome || !notas || notas.length === 0) {
+            return res.status(400).send("Nome e notas são obrigatórios");
         }
 
-        const = situacao = nota
-        situacao = nota >= 6 ? "Aprovado" : "Reprovado"; 
+        const soma = notas.reduce((valorAcumulado, nota) => valorAcumulado + nota);
+        const media = soma / notas.length;
+        const situacao = media >= 6 ? "Aprovado" : "Reprovado";
+
+        const mediaFinal = { nome, notas, soma, media, situacao };
+        isNaN(mediaFinal.media) ? res.status(400).send("Erro: Notas inválidas"):
+        res.status(200).json(mediaFinal);
+
+        let medias = []; //array para armazenar as médias
+            const data = fs.readFileSync('medias.json', 'utf-8'); //lê o arquivo medias.json
+            medias = JSON.parse(data);       
+
+        res.status(201).json(mediaFinal);
 
     } catch (error) { //tratamento de erro
         console.error("Erro ao processar a requisição:", error);
@@ -28,4 +40,3 @@ app.listen(PORT, () => {  //inicia o servidor
 
 
 });
-
