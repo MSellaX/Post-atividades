f118ca54d56ca13573a84bc807a8d063e81e7bd9
unknown <you@example.com>
2025-09-16 13:58:13 -0300

    exc4

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 //atividade 3
 
-const express = require('express'); 
+const express = require('express'); //importa o express
 const app = express();
 const PORT = 8081;
-const fs = require('fs');
+const fs = require('fs'); //importa o filesystem
 
-app.use(express.json());
+app.use(express.json());//middleware para interpretar JSON no corpo da requisição
 
 
 app.post(`/soma`, (req, res ) => { // Rota para adicionar usuários
     try {
         const {numeros} = req.body; //extrai numeros do corpo da requisição
         console.log(numeros);
-        const soma = numeros.reduce((valorAcumulado, atual) => valorAcumulado + atual, 0);
-       
-        if(Number, isNaN(numeros)){
-            return res.status(400).send("coloque apenas numeros ");
-        }
-        res.status(200).json({resultado: soma});
+
+        const numerosValidos = numeros.filter(letras => !isNaN(Number(letras)));//filtra apenas os numeros válidos
+
+        const soma = numerosValidos.reduce((valorAcumulado, atual) => valorAcumulado + Number(atual), 0);//realiza a soma dos numeros válidos
+
+        res.status(200).json({resultado: soma});//retorna o resultado da soma
 
     } catch (error) { //tratamento de erro
         console.error("Erro ao processar a requisição:", error);
